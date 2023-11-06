1445d98e846b11a8b8f0ccb7711b3f6b1d486d3b
JulianCastillo14 <juliancast140502@gmail.com>
2023-11-06 12:20:49 -0500

    Ajustando tamaño de los logos de las marcas

---
+++ src/frontend/components/Marcas.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Marcas.css'
+const Marcas = () => {
+  return (
+    <div id='Marcas'>
+        <button className='marca Chevrolet' ></button>
+        <button className='marca Ford'></button>
+        <button className='marca Toyota'></button>
+        <button className='marca Nissan'></button>
+        <button className='marca BMW'></button>
+    </div>
+  )
+}
+
+export default Marcas
--- src/frontend/screens/Inicio.jsx
+++ src/frontend/screens/Inicio.jsx
@@ -1,11 +1,13 @@
 import HeaderSB from '../components/Header.jsx'
 import OpcionesSB from '../components/Opciones-nav.jsx'
 import Carrusel from '../components/Carrusel.jsx'
+import Marcas from '../components/Marcas.jsx'
 import React, { useState } from 'react'
 import imagen1 from '../assets/images/Inicio/joy-sedan-azul-neptuno.jpg'
 import imagen2 from '../assets/images/Inicio/tracker-turbo.png';
 import imagen3 from '../assets/images/Inicio/colorado.png';
 import imagen4 from '../assets/images/Inicio/N400.jpg';
+import '../styles/Inicio.css'
 
 const Inicio = () => {
   const [showOpciones, setShowOpciones] = useState(false);
@@ -21,6 +23,10 @@ const Inicio = () => {
       <HeaderSB onToggleOpciones={handleToggleOpciones}></HeaderSB>
       {showOpciones && <OpcionesSB></OpcionesSB>}
       <Carrusel imagenes={imagenesInicio} />
+      <section id='marcas'>
+        <h3>Marcas</h3>
+      </section>
+      <Marcas></Marcas>
     </div>
   )
 }
