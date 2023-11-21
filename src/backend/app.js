0531fefe66d89ffbd1272fa5b13c6696415a7440
JulianCastillo14 <juliancast140502@gmail.com>
2023-11-21 09:50:14 -0500

    Ajustando estilos del Header y SignUp

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const {pool} = require("./db.js")
+const port = 3000
 
 app.get('/api/data', async (req, res) => {
     const result = await pool.query('SELECT * FROM Marca');
     console.log(result)
 });
 
-app.listen(3000)
-console.log('Servidor escuchando en el puerto:', 3000)
+app.listen(port)
+console.log('Servidor escuchando en el puerto:', port)
