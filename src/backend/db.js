2cbb4baccc51508b1754cb42d4c6a7a7c0a7ff20
JulianCastillo14 <juliancast140502@gmail.com>
2024-05-18 19:50:16 -0500

    Desplegando el proyecto con docker-compose

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: 'sacars-container-db',
   user: 'root',
   password: '1423',
   database: 'dbSacars',
